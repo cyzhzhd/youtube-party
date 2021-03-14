e1e560ae8521968ceaae7150c1728ab799f3d10b
ehyun <cyzhzhd@gmail.com>
2021-03-14 14:09:42 +0900

    feature:infinite scroll on PartyList
    <description>
    - Intersection Observer Web API를 사용하여 Intersection observer 구현.
    - Interserction observer hook을 만들어 재사용성 향상.
    <issue-number>
    https://github.com/cyzhzhd/youtube-party-platform/issues/11

---
+++ src/view/main/PartyList.tsx
@@ -0,0 +1,58 @@
+import React, { ReactElement, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import '../../assets/css/PartyList.css';
+import CeatePartyBox from './CreatePartyBox';
+import coverImage from '../../assets/images/cover.png';
+import { PartyListResponse } from '../../types';
+import { useQuery } from '@apollo/client';
+import { GET_PARTY_LIST } from '../../queries/party';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
+
+const PAGENATION_LIMIT = 20;
+interface Props {
+  createParty: (string) => void;
+}
+export default function PartyList({ createParty }: Props): ReactElement {
+  const { data, error, loading, fetchMore } = useQuery(GET_PARTY_LIST, {
+    variables: { limit: PAGENATION_LIMIT },
+  });
+
+  const observerRef = useRef<HTMLDivElement | null>(null);
+  function fetchMoreOnIntersect() {
+    if (data?.parties.hasMore) {
+      fetchMore({
+        variables: { cursor: data?.parties.cursor },
+      });
+    }
+  }
+  useIntersectionObserver({ observerRef, action: fetchMoreOnIntersect });
+
+  if (error) return <Error />;
+  if (loading) return <Loading />;
+  return (
+    <ul className="party-list">
+      <li className="partyList-parties-wrapper">
+        <CeatePartyBox {...{ createParty }} />
+        {data?.parties.parties.map((party: PartyListResponse) => (
+          <div key={party._id} className="partyList-party">
+            <Link to={`/partyRoom/${party._id}`}>
+              <div className="partyList-cover-image">
+                <img alt="coverImage" src={coverImage} />
+              </div>
+              <div className="partyList-details-wrapper">
+                <div className="partyList-detail">
+                  <div>{party.partyName}</div>
+                  <div>{party.uid}</div>
+                  <div>{party.numUsers}</div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </li>
+      <div ref={observerRef}></div>
+    </ul>
+  );
+}
