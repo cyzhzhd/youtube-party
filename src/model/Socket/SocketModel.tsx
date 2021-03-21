12de813e376c367412701acddda38e763af54c3f
ehyun <cyzhzhd@gmail.com>
2021-03-21 22:12:16 +0900

    etc:make the socketModel.tsx more readable
    <description>
    - useEffect에는 이름이 없기 때문에 무엇을 하는 코드인지 파악하기 어렵다고 생각.
    - 함수를 이용하여 useEffect에서 실행되는 코드가 어떤 것인지 더 명확하게 변경
    <issue-number>

--- src/model/Socket/SocketModel.tsx
+++ src/model/Socket/SocketModel.tsx
@@ -2,136 +2,142 @@ import { useReactiveVar } from '@apollo/client';
 import { useEffect } from 'react';
 import { io } from 'socket.io-client';
 import {
-  sessionIdVar,
   socketQueueVar,
   messagesVar,
   videoListVar,
-  videoTimeVar,
+  currentVideoTimeVar,
   isTimeUpToDateVar,
-  videoIdVar,
+  currentVideoIdVar,
   userVar,
 } from '../../cache';
 import { generateRandomId } from '../../helper/generateRandomId';
 import { ChatMsg, PartyResponse, VideoInfo } from '../../types';
 
-const url = process.env.REACT_APP_API_HOST as string;
-const socket = io(url, {
+const SERVER_URL = process.env.REACT_APP_API_HOST as string;
+const socket = io(SERVER_URL, {
   reconnectionDelay: 10000,
   transports: ['websocket'],
 }).connect();
 
 export default function initSocket(): void {
-  const videoId = useReactiveVar(videoIdVar);
+  const currentVideoId = useReactiveVar(currentVideoIdVar);
   const queue = useReactiveVar(socketQueueVar);
   const msgs = useReactiveVar(messagesVar);
   const videoList = useReactiveVar(videoListVar);
-  const videoTime = useReactiveVar(videoTimeVar);
+  const currentVideoTime = useReactiveVar(currentVideoTimeVar);
   const isTimeUpToDate = useReactiveVar(isTimeUpToDateVar);
-  const userData = useReactiveVar(userVar);
+  const user = useReactiveVar(userVar);
 
-  /**
-   * once user joined the website, get a session Id from server
-   */
   useEffect(() => {
-    socket.on('sessionId', (data: string) => {
-      console.log('userData', userData);
-      if (userData) {
-        userVar({ ...userData, sessionId: data });
-      } else {
+    socket.once('sessionId', (sessionId: string) => {
+      initUser();
+
+      function initUser() {
+        user ? userVar({ ...user, sessionId }) : createAnonymouseUser();
+      }
+      function createAnonymouseUser() {
         userVar({
           nickName: generateRandomId(),
-          sessionId: data,
+          sessionId,
         });
       }
-      sessionIdVar(data);
     });
   }, []);
 
   useEffect(() => {
-    socket.on('deliverVideoTime', (data: VideoInfo) => {
-      console.log('deliverVideoTime', data);
-      if (data.videoId !== videoId) {
-        videoIdVar(data.videoId);
+    socket.once('deliverVideoTime', ({ videoId: deliveredVideoId, time }: VideoInfo) => {
+      synchronizeVideo();
+
+      function synchronizeVideo() {
+        if (deliveredVideoId !== currentVideoId) {
+          currentVideoIdVar(deliveredVideoId);
+        }
+        currentVideoTimeVar(time);
+        isTimeUpToDateVar(false);
       }
-      videoTimeVar(data.time);
-      isTimeUpToDateVar(false);
     });
-  }, [videoId, isTimeUpToDate, videoTime]);
+  }, [currentVideoId, isTimeUpToDate, currentVideoTime]);
 
   useEffect(() => {
-    socket.on('deliverVideoId', (data: { videoId: string }) => {
-      console.log('deliverVideoId', data);
-      videoIdVar(data.videoId);
+    socket.once('deliverVideoId', ({ videoId }: { videoId: string }) => {
+      synchronizeVideoId();
+
+      function synchronizeVideoId() {
+        currentVideoIdVar(videoId);
+      }
     });
   });
 
-  // when video is added in the list
   useEffect(() => {
-    socket.once('deliverPartyDetail', (partyDetail: PartyResponse) => {
-      console.log(partyDetail);
-      if (!videoId) videoIdVar(partyDetail.videos[0]?.vid);
-      videoListVar(partyDetail.videos);
+    socket.once('deliverPartyDetail', ({ videos }: PartyResponse) => {
+      synchronizeVideoList();
+
+      function synchronizeVideoList() {
+        if (!currentVideoId) currentVideoIdVar(videos[0]?.vid);
+        videoListVar(videos);
+      }
     });
   }, [videoList]);
 
-  /**
-   * receiving chat message
-   */
   useEffect(() => {
     socket.once('deliverChat', (data: ChatMsg) => {
-      console.log(data);
-      messagesVar([...msgs, { ...data }]);
+      addReceivingMessageOnMessages();
+
+      function addReceivingMessageOnMessages() {
+        messagesVar([...msgs, { ...data }]);
+      }
     });
   }, [msgs]);
 
-  // when there is a request for socket server
   useEffect(() => {
-    if (queue.length < 1) return;
-
-    const action = queue[0];
-    console.log(queue);
-    switch (action.type) {
-      case 'joinPartyRoom':
-        socket.emit('joinPartyRoom', {
-          partyId: action.partyId,
-        });
-        break;
-      case 'sendMsg':
-        socket.emit('sendMsg', {
-          uid: userData?.uid,
-          nickName: userData?.nickName,
-          content: action.content,
-          partyId: action.partyId,
-        });
-        break;
-      case 'updateVideoList':
-        socket.emit('updateVideoList', {
-          uid: userData?.uid,
-          nickName: userData?.nickName,
-          add: action.add,
-          partyId: action.partyId,
-          videoId: action.videoId,
-        });
-        break;
-      case 'syncVideoTime':
-        socket.emit('syncVideoTime', {
-          uid: userData?.uid,
-          nickName: userData?.nickName,
-          partyId: action.partyId,
-          videoId: action.videoId,
-          time: action.time,
-        });
-        break;
-      case 'syncVideoId':
-        socket.emit('syncVideoId', {
-          uid: userData?.uid,
-          nickName: userData?.nickName,
-          partyId: action.partyId,
-          videoId: action.videoId,
-        });
-        break;
+    if (queue.length > 1) {
+      emitFirstActionInQueue();
+      socketQueueVar(queue.slice(1));
     }
 
-    socketQueueVar(queue.slice(1));
+    function emitFirstActionInQueue() {
+      const action = queue[0];
+      switch (action.type) {
+        case 'joinPartyRoom':
+          socket.emit(action.type, {
+            partyId: action.partyId,
+          });
+          break;
+        case 'sendMsg':
+          socket.emit(action.type, {
+            uid: user?.uid,
+            nickName: user?.nickName,
+            content: action.content,
+            partyId: action.partyId,
+          });
+          break;
+        case 'updateVideoList':
+          socket.emit(action.type, {
+            uid: user?.uid,
+            nickName: user?.nickName,
+            add: action.add,
+            partyId: action.partyId,
+            videoId: action.videoId,
+          });
+          break;
+        case 'syncVideoTime':
+          socket.emit(action.type, {
+            uid: user?.uid,
+            nickName: user?.nickName,
+            partyId: action.partyId,
+            videoId: action.videoId,
+            time: action.time,
+          });
+          break;
+        case 'syncVideoId':
+          socket.emit(action.type, {
+            uid: user?.uid,
+            nickName: user?.nickName,
+            partyId: action.partyId,
+            videoId: action.videoId,
+          });
+          break;
+      }
+    }
   }, [queue]);
 }
